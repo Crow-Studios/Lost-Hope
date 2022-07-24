aae0cd7f509d4688f05d9bb0eadc035da0aa1cd9
SilenceIsFatto <78276788+SilenceIsFatto@users.noreply.github.com>
2022-07-24 12:20:08 +0100

    packed mod

---
+++ @Lost_Hope/mod.cpp
@@ -0,0 +1,14 @@
+ name 			= "Lost Hope"; // Name of your mod
+ author 		= "Silence, Zelo"; // Affects Arma 3 Launcher, when the mod are loaded as local
+ logo 			= "Lost_Hope\crow_studios_co.paa"; // Logo displayed in the main menu
+ logoOver 	= "Lost_Hope\crow_studios_co.paa"; // When the mouse is over, in the main menu
+ tooltip 		= "All Hope Is Lost";
+ tooltipOwned = "Silence, Zelo "; // Tool tip displayed when the mouse is left over, in the main menu
+ picture 		= "Lost_Hope\crow_studios_co.paa"; // Picture displayed from the expansions menu. Optimal size is 2048x1024
+ actionName = "Discord Link";
+ action 		= "https:\\discord.gg\PTbEk8y"; // Website URL, that can be accessed from the expansions menu
+ overview 		= ""; // Supports structured text
+ hideName 		= 0; // Hide the extension name
+ hidePicture	= 0;	// Hide the extension menu
+ dlcColor[] 	= {0.23,0.39,0.30,1}; // Color used for DLC stripes and backgrounds (RGBA)
+ logoSmall 		= "Lost_Hope\crow_studios_co.paa"; // Display in creative lists, next to the entities added by the mod
