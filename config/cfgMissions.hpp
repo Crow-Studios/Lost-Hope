986ad7e47a49bfa2686f0fffa321299ac9e35589
SilenceIsFatto <78276788+SilenceIsFatto@users.noreply.github.com>
2022-08-13 20:09:55 +0100

    cfgMissions fix

--- config/cfgMissions.hpp
+++ config/cfgMissions.hpp
@@ -1,13 +1,81 @@
 class CfgMissions
 {
+	class Campaigns
+	{
+		class LostHope
+		{
+			directory = "Lost_Hope\missions";
+		};
+	};
 	class MPMissions
 	{
 		// Lost Hope - Malden 2035
-		class Lost_Hope_Mission_Malden
+		class LostHopeMalden
 		{
 			briefingName = "Lost Hope - Malden";
-			directory = "Lost_Hope\missions\Lost_Hope.Malden";
+			directory = "Lost_Hope\missions\LostHope.Malden";
 			author = "Lost Hope Dev Team";
 		};
 	};
 };
+
+/*
+class CfgMainMenuSpotlight
+{
+	class LostHope
+	{
+		text = "All Hope Is Lost"; // Text displayed on the square button, converted to upper-case
+		textIsQuote = 1; // 1 to add quotation marks around the text
+		picture = ""; // Square picture, ideally 512x512
+		video = ""; // Video played on mouse hover
+		action = "";
+		actionText = ""; // Text displayed in top left corner of on-hover white frame
+		condition = "true"; // Condition for showing the spotlight
+	};
+	class ApexProtocol
+	{
+		condition = "false";
+	};
+	class Bootcamp
+	{
+		condition = "false";
+	};
+	class Orange_Campaign
+	{
+		condition = "false";
+	};
+	class Orange_CampaignGerman
+	{
+		condition = "false";
+	};
+	class Orange_Showcase_IDAP
+	{
+		condition = "false";
+	};
+	class Orange_Showcase_LoW
+	{
+		condition = "false";
+	};
+	class Showcase_TankDestroyers
+	{
+		condition = "false";
+	};
+	class Tacops_Campaign_01
+	{
+		condition = "false";
+	};
+	class Tacops_Campaign_02
+	{
+		condition = "false";
+	};
+	class Tacops_Campaign_03
+	{
+		condition = "false";
+	};
+	class Tanks_Campaign_01
+	{
+		condition = "false";
+	};
+
+};
+*/
